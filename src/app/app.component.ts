326c69d83b79f57b255966ca2ec2c89fb4a152af
christoph-hue <christoph.huesson@gmx.net>
2022-04-07 19:53:51 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { NgModule, Component, enableProdMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
+
+import { DxDataGridModule } from 'devextreme-angular';
+import CustomStore from 'devextreme/data/custom_store';
+
+
+@Component({
+    styleUrls: ['./app.component.css'],
+  selector: 'demo-app',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  dataSource: any = {};
+
+  constructor(httpClient: HttpClient) {
+    function isNotEmpty(value: any): boolean {
+      return value !== undefined && value !== null && value !== '';
+    }
+    this.dataSource = new CustomStore({
+      key: 'OrderNumber',
+      load(loadOptions: any) {
+        let params: HttpParams = new HttpParams();
+        [
+          'skip',
+          'take',
+          'requireTotalCount',
+          'requireGroupCount',
+          'sort',
+          'filter',
+          'totalSummary',
+          'group',
+          'groupSummary',
+        ].forEach((i) => {
+          if (i in loadOptions && isNotEmpty(loadOptions[i])) { params = params.set(i, JSON.stringify(loadOptions[i])); }
+        });
+        return httpClient.get('https://js.devexpress.com/Demos/WidgetsGalleryDataService/api/orders', { params })
+          .toPromise()
+          .then((data: any) => ({
+            data: data.data,
+            totalCount: data.totalCount,
+            summary: data.summary,
+            groupCount: data.groupCount,
+          }))
+          .catch((error) => { throw 'Data Loading Error'; });
+      },
+    });
+  }
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    DxDataGridModule,
+    HttpClientModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
